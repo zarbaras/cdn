c1a90f3f2d9d41be4ab9cff35b6a61a829f50b72
zarbaras <zarbaras@hotmail.com>
2025-05-11 22:10:41 +0300

    Update chatbotdesign.js

--- chatbotdesign.js
+++ chatbotdesign.js
@@ -373,10 +373,10 @@
     `;
     
     chatContainer.innerHTML = newConversationHTML + chatInterfaceHTML;
+    
     const poweredByLink = chatContainer.querySelector('.chat-footer a');
 if (poweredByLink) {
-    poweredByLink.style.display = 'none';
-}
+    poweredByLink.style.display = 'none';}
 
     const toggleButton = document.createElement('button');
     toggleButton.className = `chat-toggle${config.style.position === 'left' ? ' position-left' : ''}`;
