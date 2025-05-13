306901b325fb6000373532e93c194bba226a13ba
zarbaras <zarbaras@hotmail.com>
2025-05-13 14:27:05 +0300

    Update chatbotdesign.js

--- chatbotdesign.js
+++ chatbotdesign.js
@@ -295,10 +295,7 @@
             name: '',
             welcomeText: '',
             responseTimeText: '',
-            poweredBy: {
-                text: 'Powered by n8n',
-                link: 'https://n8n.partnerlinks.io/m8a94i19zhqq?utm_source=nocodecreative.io'
-            }
+           
         },
         style: {
             primaryColor: '',
